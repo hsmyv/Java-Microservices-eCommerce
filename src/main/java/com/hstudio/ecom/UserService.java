774b2d3cb1437f9af249bb2b41d548c6637b5beb
Hasan <hsmusayev@gmail.com>
2026-04-25 20:03:07 +0400

    feat: add repository

---
+++ src/main/java/com/hstudio/ecom/UserService.java
@@ -0,0 +1,45 @@
+package com.hstudio.ecom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<User> fetchAllUsers(){
+        return userList;
+    }
+    public List<User> addUser(User user) {
+        user.setId(nextId++);
+        userList.add(user);
+        return userList;
+    }
+
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public boolean updateUser(Long id, User updatedUser){
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser.setFirstName(updatedUser.getFirstName());
+                    existingUser.setLastName(updatedUser.getLastName());
+                    return true;
+                }).orElse(false);
+    }
+}
